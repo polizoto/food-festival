842265f888050fc4367d826ef6de1102717f7b6f
polizoto <polizoto@hotmail.com>
2022-03-23 16:41:22 -0400

    updated the webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -46,5 +46,9 @@ module.exports = {
           analyzerMode: "static", // the report outputs to an HTML file in the dist folder
         })
       ],
-      mode: 'development'
+      mode: 'development',
+
+      devServer: {
+        static: "./",
+      },
 };
